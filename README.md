ef5bafcf7a9b2dcc3019279afa49cc02e2bd52f0
Jack <jackgr83@gmail.com>
2024-12-04 09:18:20 -0500

    add logic for conflicting rates

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# reimbursement-calculator
+# Travel Reimbursement Calculator
+
+### Introduction
+This code calculates the total travel reimbursement amount for a given set of projects. A project contains a start date, end date, and a type of city based on cost of living (high vs low). A set can contain one or multiple projects.
+
+The reimbursement calculator abides by the following rules:
+- First day and last day of a project, or sequence of projects, is a travel day.
+- Any day in the middle of a project, or sequence of projects, is considered a full day.
+- If there is a gap between projects, then the days on either side of that gap are travel days.
+- If two projects push up against each other, or overlap, then those days are full days as well.
+- Any given day is only ever counted once, even if two projects are on the same day.
+- A travel day is reimbursed at a rate of 45 dollars per day in a low cost city.
+- A travel day is reimbursed at a rate of 55 dollars per day in a high cost city.
+- A full day is reimbursed at a rate of 75 dollars per day in a low cost city.
+- A full day is reimbursed at a rate of 85 dollars per day in a high cost city.
+
+### Requirements
+A working installation of Python 3.x
+
+### Instructions
+Follow these steps to use the reimbursement calculator:
+
+1. Clone this repository locally:
+```
+git clone https://github.com/jackgr83/reimbursement-calculator.git
+```
+
+2. Navigate to the project root directory:
+```
+cd ./reimbursement-calculator
+```
+
+3. Run the reimbursement calculator via python:
+```
+python calculate.py
+```
+
+4. Follow the prompts to define a set. Enter a project or multiple projects. Each project must contain a start date and end date in month/day/year format with the year abbreviated to the last two digits (e.g. 9/1/15), and a city type. After entering in the last project, responding no at the last prompt will automatically run the reimbursement calculator on the data provided. The calculator will output the amount of reimbursement for each date and the total reimbursement amount.
--- calculate.py
+++ calculate.py
@@ -5,17 +5,22 @@ full_day = {"l": 75, "h": 85}
 
 def calculate(set):
     total = 0
-
     current_dt = datetime.strptime(set[0]['start_dt'], '%m/%d/%y').date()
 
-    for proj in set:
+    for pidx, proj in enumerate(set):
         city_tp = proj['city_tp']
+        city_tp_last = set[pidx-1]['city_tp']
         start_dt = datetime.strptime(proj['start_dt'], '%m/%d/%y').date()
 
         # Add gap days between projects to total
-        for day in (current_dt + timedelta(n) for n in range((start_dt - current_dt).days)):
-            print(f"Adding {current_dt} to total cost as a travel day in between projects")
-            total += travel_day[city_tp]
+        if pidx > 0:
+            for day in (current_dt + timedelta(n) for n in range((start_dt - current_dt).days)):
+                if city_tp == "l" and city_tp_last == "h":
+                    print(f"Adding {travel_day[city_tp_last]} to total cost for {current_dt} as a gap travel day for a {city_tp_last.upper()} cost city.")
+                    total += travel_day[city_tp_last]
+                else:
+                    print(f"Adding {travel_day[city_tp]} to total cost for {current_dt} as a gap travel day for a {city_tp.upper()} cost city.")
+                    total += travel_day[city_tp]
         
         end_dt = datetime.strptime(proj['end_dt'], '%m/%d/%y').date()
         num_days = (end_dt - start_dt).days
@@ -23,31 +28,77 @@ def calculate(set):
         for i, day in enumerate(start_dt + timedelta(n) for n in range(num_days + 1)):
             first_day = 0
             last_day = num_days
-
-
             if i == first_day or i == last_day:
                 # Account for project days that overlap
-                if current_dt > day:
-                    print(f"Removing {day} as a travel day and replacing it with a full day since it overlaps with last project")
-                    total -= travel_day[city_tp]
-                    total += full_day[city_tp]
+                if current_dt > day and pidx > 0:
+                    print(f"Removing {travel_day[city_tp_last]} from total cost for {day} as a travel day in a {city_tp_last.upper()} cost city.")
+                    # Remove day of last project from total
+                    total -= travel_day[city_tp_last]
+                    # If conflicting rates between projects, use the high cost rate
+                    if city_tp == "l" and city_tp_last == "h":
+                        print(f"Adding {full_day[city_tp_last]} to total cost for {day} as a full day in a {city_tp_last.upper()} cost city.")
+                        total += full_day[city_tp_last]
+                    else:
+                        print(f"Adding {full_day[city_tp]} to total cost for {day} as a full day in a {city_tp.upper()} cost city.")
+                        total += full_day[city_tp]
                 else:
-                    print(f"Adding {day} to total cost as a travel day")
+                    print(f"Adding {travel_day[city_tp]} to total cost for {day} as a travel day in a {city_tp.upper()} cost city")
                     total += travel_day[city_tp]
             else:
                 # Only add day to total if not overlapping
-                if current_dt < day:
-                    print(f"Adding {day} to total cost as a full day")
+                if current_dt <= day:
+                    print(f"Adding {full_day[city_tp]} to total cost for {day} as a full day in a {city_tp.upper()} cost city")
                     total += full_day[city_tp]
+                elif pidx > 0:
+                    # If conflicting rates between projects, use the high cost rate
+                    if city_tp == "h" and city_tp_last == "l":
+                        print(f"Removing {full_day[city_tp_last]} from total cost for {day} as a full day in a {city_tp_last.upper()} cost city.")
+                        total -= full_day[city_tp_last]
+                        print(f"Adding {full_day[city_tp]} to total cost for {day} as a full day in a {city_tp.upper()} cost city.")
+                        total += full_day[city_tp]
+
         current_dt = end_dt + timedelta(days=1)
+        
     print(f"Total Reimbursement Cost: {total}")
 
-def get_projects():  
-    start_dt = input("Enter project start date in format M/D/Y: ")
-    end_dt = input("Enter project end date in format M/D/Y: ")
-    city_tp = input("High or Low Cost city? (h/l): ")
+def get_projects():
+    while True:
+        try:
+            start_dt = input("Enter project START date in format M/D/Y: ")
+            check_stdate = datetime.strptime(start_dt, '%m/%d/%y').date()
+            break
+        except ValueError:
+            print("Please enter date in M/D/Y format. Use only the last two digits for the year. Example: 9/1/15")
+            continue
+    while True:
+        try:  
+            end_dt = input("Enter project END date in format M/D/Y: ")
+            check_endate = datetime.strptime(end_dt, '%m/%d/%y').date()
+            if check_stdate > check_endate:
+                print("Please enter an end date on or after the previously entered start date.")
+                continue
+            break
+        except ValueError:
+            print("Please enter date in M/D/Y format. Use only the last two digits for the year. Example: 9/1/15")
+            continue
+    while True:
+        city_tp = input("High or Low Cost city? (h/l): ")
+        if city_tp.lower() not in ['h', 'l']:
+            print("Please enter city type as either h or l.")
+            continue
+        else:
+            break
+              
     set.append({"start_dt": start_dt, "end_dt": end_dt, "city_tp": city_tp})
-    if (input("Enter another project in set? (y/n):")) == "n":
+
+    while True:
+        more_projects = input("Enter another project in set? (y/n):")
+        if more_projects.lower() not in ['y', 'n']:
+            print("Please enter either y or n.")
+            continue
+        else:
+            break
+    if (more_projects) == "n":
         return
     else:
         get_projects()
@@ -56,6 +107,5 @@ if __name__ == "__main__":
     print("Define a Set by entering Projects in chronological order...")
     set = []
     get_projects()
-
     calculate(set)
     
